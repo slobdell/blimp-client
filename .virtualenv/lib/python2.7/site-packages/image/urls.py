4f163fb220dc3e7f6a893dbb8497b997a962065c
Scott Lobdell <slobdell@uber.com>
2015-09-07 23:53:41 -0700

    dont include virtualenv

--- .virtualenv/lib/python2.7/site-packages/image/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    'image.views',
-    url(r'^crosshair$', 'crosshair'),
-    url(r'^(?P<path>.+)/(?P<token>[\w_=&]+)$', 'image'),
-)
